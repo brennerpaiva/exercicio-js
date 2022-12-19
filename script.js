1bc52b3c0eef7ba41af37645c52ac50f40174599
brennerpaiva <114958953+brennerpaiva@users.noreply.github.com>
2022-12-19 11:54:09 -0300

    exercicio-js

---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = 13//data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora < 12) {
+        //Bom dia
+        img.src = "imagens/manhã-pq.png"
+        document.body.style.background = "rgb(202, 172, 127)"
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+        img.src = "imagens/tarde-pq.png"
+        
+    } else {
+        //Boa noite
+        img.src = "imagens/noite-pq.png"
+        document.body.style.background = "#515154"
+    }
+}
