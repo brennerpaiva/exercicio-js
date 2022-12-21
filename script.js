86129e57a2cae3d49e4d2e1156cf81015d62a42c
brennerpaiva <114958953+brennerpaiva@users.noreply.github.com>
2022-12-20 22:25:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
     var data = new Date()
-    var hora = 13//data.getHours()
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora < 12) {
